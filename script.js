e1d96a49238ead5461bb6d013afcb8b82fb2598c
hibi221b <hibi221b@hibi221b-2.local>
2020-11-02 22:02:59 +0900

    test

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const renderPlaces = (places) => {
     let modelIndex = 0;
     let models = [
         {
-            position: "-12 4 10",
+            position: "-20 4 10",
             color: "red",
             scale: "7 7 7"
         }
